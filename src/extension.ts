bfc6b212946f74da3a9f7aa1d59593140d6e5fd8
aki <lala.akira@gmail.com>
2023-05-08 08:03:31 +0900

    feat: Add openFileInCurrentWindow command

--- src/extension.ts
+++ src/extension.ts
@@ -34,6 +34,21 @@ const selectRepository = async (): Promise<vscode.Uri | undefined> => {
   return vscode.Uri.file(repositoryPath);
 };
 
+const getFilesByRepository = async (repository: string): Promise<readonly File[]> => {
+  const rootPath = await getRoot();
+  if (!rootPath) {
+    return [];
+  }
+
+  const repositoryPath = path.join(rootPath, repository);
+  const {stdout} = await exec('git ls-files', { cwd: repositoryPath });
+  return stdout.split('\n').filter((x) => x).map((filePath) => ({
+    label: path.basename(filePath),
+    description: filePath,
+    path: path.join(repositoryPath, filePath),
+  }));
+};
+
 const open = async (newWindow = false) => {
   const uri = await selectRepository();
   if (!uri) {
@@ -50,7 +65,7 @@ const openInBrowser = async () => {
   }
 
   const webUri = 'https://' + uri.path.split('/').slice(-3).join('/');
-  vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(webUri));
+  return vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(webUri));
 };
 
 const addToWorkSpace = async () => {
@@ -63,13 +78,55 @@ const addToWorkSpace = async () => {
   vscode.workspace.updateWorkspaceFolders(position, null, { uri });
 };
 
+type Repository = {
+  label: string;
+};
+
+type File = {
+  label: string;
+  description: string;
+  path: string;
+};
+
+const openFileInCurrentWindow = async () => {
+  const quickPick = vscode.window.createQuickPick<Repository | File>();
+
+  quickPick.onDidAccept(async () => {
+    const selectedItem = quickPick.selectedItems[0];
+    if (!selectedItem) {
+      return;
+    }
+
+    if (quickPick.step === 1) {
+      quickPick.busy = true;
+      quickPick.value = "";
+      quickPick.step = 2;
+      quickPick.placeholder = "Select a File";
+      quickPick.items = await getFilesByRepository(selectedItem.label);
+      quickPick.matchOnDescription = true;
+      quickPick.busy = false;
+    } else {
+      vscode.commands.executeCommand('vscode.open', vscode.Uri.parse((selectedItem as File).path));
+    }
+  });
+
+  quickPick.totalSteps = 2;
+  quickPick.busy = true;
+  quickPick.value = "";
+  quickPick.placeholder = "Select a Repository";
+  quickPick.step = 1;
+  quickPick.show();
+  quickPick.items = (await getRepositories()).map((x) => ({ label: x }));
+  quickPick.busy = false;
+};
+
 export async function activate(context: vscode.ExtensionContext) {
   if (!await getRoot()) {
     vscode.window.showWarningMessage('ghq is not installed.');
     return;
   }
 
-  context.subscriptions.push(vscode.commands.registerCommand('ghq.open', () => {
+  context.subscriptions.push(vscode.commands.registerCommand('ghq.open', async () => {
     return open();
   }));
   context.subscriptions.push(vscode.commands.registerCommand('ghq.openInNewWindow', () => {
@@ -80,6 +137,9 @@ export async function activate(context: vscode.ExtensionContext) {
   }));
   context.subscriptions.push(vscode.commands.registerCommand('ghq.addToWorkSpace', () => {
     return addToWorkSpace();
+  }));
+  context.subscriptions.push(vscode.commands.registerCommand('ghq.openFileInCurrentWindow', () => {
+    return openFileInCurrentWindow();
   }));
 }
 // This method is called when your extension is deactivated
